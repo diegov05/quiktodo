4719e1ab51ffcfa303c91f91cc21af8e013cb02b
Diego Vargas <diegovs0504@gmail.com>
2023-06-15 20:29:22 -0400

    Made progress in Todo Resolver

--- backend/src/resolvers/Todo.ts
+++ backend/src/resolvers/Todo.ts
@@ -1,3 +1,4 @@
+import { Todo as PrismaTodo } from '@prisma/client';
 import { Arg, Ctx, Field, ID, Int, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { MyContext } from 'src/types';
 
@@ -53,5 +54,53 @@ export class TodoResolver {
             }
         })
         return todo
+    }
+
+    // @Mutation(() => Todo)
+    // async updateTodo(
+    //     @Arg('id') id: number,
+    //     @Arg('task', () => String, { nullable: true }) todoname: string | undefined,
+    //     @Arg('isCompleted', () => Boolean, { nullable: true }) isCompleted: boolean | undefined,
+    //     @Arg('deadline', () => Boolean, { nullable: true }) isCompleted: boolean | undefined,
+    //     @Ctx() { prisma }: MyContext
+    // ): Promise<PrismaTodo | null> {
+    //     const todo = await prisma.todo.findUnique({ where: { id: id } })
+    //     if (!todo) {
+    //         return null
+    //     }
+
+    //     if (todoname !== undefined && isCompleted !== undefined &&  ) {
+    //         await prisma.todo.update({
+    //             where: { id: id },
+    //             data: { task: taks, password: password }
+    //         })
+    //     } else if (todoname !== undefined) {
+    //         await prisma.todo.update({
+    //             where: { id: id },
+    //             data: { todoname: todoname }
+    //         })
+    //     } else if (password !== undefined) {
+    //         await prisma.todo.update({
+    //             where: { id: id },
+    //             data: { password: password }
+    //         })
+    //     }
+
+    //     return todo
+    // }
+
+    @Mutation(() => Boolean)
+    async deleteTodo(
+        @Arg("id") id: number,
+        @Ctx() { prisma }: MyContext
+    ): Promise<Boolean> {
+        const todo = await prisma.todo.findUnique({ where: { id: id } })
+
+        if (!todo) {
+            return false
+        }
+
+        await prisma.todo.delete({ where: { id: id } })
+        return true
     }
 }
