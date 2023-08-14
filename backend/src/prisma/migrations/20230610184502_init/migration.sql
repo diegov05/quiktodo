efe134479700d06d1eef77ad10731240f3103cd1
Diego Vargas <diegovs0504@gmail.com>
2023-08-14 19:40:05 -0400

    Deleted the whole backend

--- backend/src/prisma/migrations/20230610184502_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Todo" ALTER COLUMN "deadline" DROP DEFAULT;
--- backend/src/types.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-
-export type MyContext = {
-    prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>
-}
